e1ab0a8b12705823089733f3d97c43a3c8cc6da4
Lucas Carvalho <lucas.cdsilva@gmail.com>
2017-02-22 22:33:45 -0400

    refatored code style

--- has-support-inputtype.js
+++ has-support-inputtype.js
@@ -1,11 +1,11 @@
-(function(){
+(function() {
   window.hasSupportInputType = function(type) {
     try {
-       var input = document.createElement('input');
-       input.type = type;
-       return (input.type == type);
+      var input = document.createElement('input');
+      input.type = type;
+      return input.type === type;
     } catch(e) {
       return false;
     }
- }
+  };
 })();
